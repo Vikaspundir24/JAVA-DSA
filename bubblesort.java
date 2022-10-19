15eebf6a173cd0dcb1973ab7fb6e8ff5c48a50bc
vikas pundir <vikaspundiruser@gmail.com>
2022-10-19 16:08:42 +0530

    sorting

---
+++ bubblesort.java
@@ -0,0 +1,13 @@
+import java.util.*
+
+class sorting {
+    public static void main(string args[])
+    {
+        int arr[] = {7 , 8 , 3 , 1, 2};
+        
+        for(int i = 0 ; i <arr.length() -1 ; i++)
+        {
+            for
+        }
+    }
+}
